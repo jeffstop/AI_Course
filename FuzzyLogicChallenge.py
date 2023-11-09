aff8f30f1c8eec93c135449d98d6e534f6c17380
jeffstop <84537804+jeffstop@users.noreply.github.com>
2023-11-09 13:04:04 -0800

    Update FuzzyLogicChallenge.py

--- FuzzyLogicChallenge.py
+++ FuzzyLogicChallenge.py
@@ -1,6 +1,10 @@
 import numpy as np
 import skfuzzy as fuzz
 
+# Accept user input
+user_input = input("Enter a value between 0 and 10: ")
+input_value = float(user_input)
+
 # Input variable; Range 0-10
 x = np.arange(0, 11, 1)
 
@@ -9,15 +13,24 @@ low = fuzz.trimf(x, [0, 0, 5])
 medium = fuzz.trimf(x, [2, 5, 8])
 high = fuzz.trimf(x, [5, 10, 10])
 
+# Get membership values for input_value
+low_degree = fuzz.interp_membership(x, low, input_value)
+medium_degree = fuzz.interp_membership(x, medium, input_value)
+high_degree = fuzz.interp_membership(x, high, input_value)
+
 # Define fuzzy rules
-rule1 = np.fmax(low, medium)
-rule2 = np.fmin(medium, high)
+rule1 = np.fmax(low_degree, medium_degree)
+rule2 = np.fmin(medium_degree, high_degree)
 
 # Apply fuzzy rules to determine the fuzzy relation between input and output
 relation = np.fmax(rule1, rule2)
 
-# Defuzzify the relation to obtain a crisp output
-output = fuzz.defuzz(x, relation, 'centroid')
+# Aggregate the fuzzy relation using the maximum operator
+aggregated = np.fmax(low, relation)
+activated = np.fmin(aggregated, medium)
+
+# Defuzzify the activated fuzzy relation to obtain a crisp output
+output = fuzz.defuzz(x, activated, 'centroid')
 
 # Display the crisp output
 print("Output: ", output)
