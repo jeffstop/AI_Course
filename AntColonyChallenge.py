529e5b27dc3d1ce7343c47bc856c88f895e67be9
jeffstop <84537804+jeffstop@users.noreply.github.com>
2023-11-09 15:47:40 -0800

    Update AntColonyChallenge.py

--- AntColonyChallenge.py
+++ AntColonyChallenge.py
@@ -1,3 +1,13 @@
 import numpy as np
 
 # Define the distance matrix
+distance_matrix = np.array([
+    [0, 2, 4, 3, 5],
+    [2, 0, 6, 7, 8],
+    [4, 6, 0, 1, 3],
+    [3, 7, 1, 0, 2],
+    [5, 8, 3, 2, 0]
+])
+
+# Number of cities
+num_cities = distance_matrix.shape[0]
