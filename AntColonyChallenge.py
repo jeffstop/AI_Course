f98139db033af6977a5d811a177e2bc012b7c8c0
jeffstop <84537804+jeffstop@users.noreply.github.com>
2023-11-10 13:20:17 -0800

    Update AntColonyChallenge.py

--- AntColonyChallenge.py
+++ AntColonyChallenge.py
@@ -51,3 +51,39 @@ def ant_colony_optimization(num_iterations):
                 # Choose the next city based on the selection probabilities
                 next_city = np.random.choice(np.arange(num_cities), p=(selection_probs / np.sum(selection_probs)))
 
+                # Update the path and visited last
+                ant_paths[ant, _+1] = next_city
+                visited.append(next_city)
+
+                # Update the distance
+                ant_distances[ant] += distance_matrix[current_city, next_city]
+
+                # Update current city
+                current_city = next_city
+
+            # Update distance to return to starting city
+            ant_distances[ant] += distance_matrix[current_city, ant_paths[ant, 0]]
+
+        # Update pheromone level based on the ant paths
+        pheromone_level *= 0.5          # Evaporation
+        for ant in range(num_ants):
+            for city in range(num_cities - 1):
+                pheromone_level[ant_paths[ant, city], ant_paths[ant, city+1]] += 1 / ant_distances[ant]
+            pheromone_level[ant_paths[ant, -1], ant_paths[ant, 0]] += 1 / ant_distances[ant]
+
+
+        # Update the best path and distance if a better solution is found
+        min_distance_idx = np.argmin(ant_distances)
+        if ant_distances[min_distance_idx] < best_distance:
+            best_distance = ant_distances[min_distance_idx]
+            best_path = ant_paths[min_distance_idx]
+
+    return best_path, best_distance
+
+# Run the Ant Colony Optimization algorithm
+num_iterations = 100
+best_path, best_distance = ant_colony_optimization(num_iterations)
+
+# Display the best path and distance
+print("Here is the best path: ", best_path)
+print("Here is the best distance: ", best_distance)
