7c07b0dc9c0723d148ddaa247d01788e9077e78b
jeffstop <84537804+jeffstop@users.noreply.github.com>
2023-11-10 12:47:06 -0800

    Update AntColonyChallenge.py

--- AntColonyChallenge.py
+++ AntColonyChallenge.py
@@ -11,3 +11,43 @@ distance_matrix = np.array([
 
 # Number of cities
 num_cities = distance_matrix.shape[0]
+
+# Number of ants
+num_ants = 10
+
+def ant_colony_optimization(num_iterations):
+    pheromone_level = np.ones((num_cities, num_cities))
+
+    # Initialize heuristic information matrix
+    heuristic_info = 1 / (distance_matrix + np.finfo(float).eps)
+
+    # Alpha and beta parameters || alpha = pheromone importance & beta = heuristic importance
+    alpha = 1.0
+    beta = 2.0
+
+    # Initialize best path and distance
+    best_distance = float('inf')
+    best_path = []
+
+    for iteration in range(num_iterations):
+
+        # Initialize ant paths and distances
+        ant_paths = np.zeros((num_ants, num_cities), dtype=int)
+        ant_distances = np.zeros(num_ants)
+
+        for ant in range(num_ants):
+
+            # Choose starting city randomly
+            current_city = np.random.randint(num_cities)
+            visited = [current_city]
+
+            # Construct the path
+            for _ in range(num_cities - 1):
+
+                # Calculate the selection probabilities
+                selection_probs = (pheromone_level[current_city] ** alpha) * (heuristic_info[current_city] ** beta)
+                selection_probs[np.array(visited)] = 0
+
+                # Choose the next city based on the selection probabilities
+                next_city = np.random.choice(np.arange(num_cities), p=(selection_probs / np.sum(selection_probs)))
+
