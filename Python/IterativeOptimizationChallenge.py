df7bada4f91b1bba71a33ed77e511b27773d8c99
Jeff <jstopp10@gmail.com>
2023-11-13 14:51:39 -0800

    Create IterativeOptimizationChallenge.py

---
+++ Python/IterativeOptimizationChallenge.py
@@ -0,0 +1,15 @@
+def function(x):
+    return x**2 + 5*x + 6
+
+def derivative(x):
+    return 2*x + 5
+
+x = 0
+learning_rate = 0.1
+num_iterations = 100
+
+for _ in range(num_iterations):
+    gradient = derivative(x)
+    x = x - learning_rate * gradient
+    
+print("the minimum value found at x = ", x)
