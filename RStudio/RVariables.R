1a314bfa39b4fd28885299c0b40a6d25cc6d41ee
Jeff <jstopp10@gmail.com>
2023-11-13 15:42:58 -0800

    Create RVariables.R

---
+++ RStudio/RVariables.R
@@ -0,0 +1,58 @@
+name <- "Jack"
+age <- 37
+name # Output: "Jack"
+age  # Output: 37
+
+yourname <- "Jeff"
+yourname
+print(yourname)
+
+text <- "very useful!"
+paste("R is", text)
+
+num1 <- 15
+num2 <- 10
+num1 + num2
+
+num <- 5
+text2 <- "Some text"
+num + text2
+
+# Assign the same value to multiple variables in one line
+var1 <- var2 <- var3 <- "Banana"
+# Print variable values
+var1
+var2
+var3
+
+myvar <- 182 # myvar is of type numeric
+myvar <- "Johnny Tyler"  # myvar is now type character(string)
+myvar
+
+x <- 12.5
+y <- 66
+x
+y
+# Print the class name of x and y
+class(x)
+class(y)
+
+i <- 1985L
+j <- 50L
+i
+j
+class(i)
+class(j)
+
+
+x <- 17i + 9
+x
+class(x)
+
+x <- "I love learning R!"
+x
+class(x)
+
+x <- TRUE
+x
+class(x)
