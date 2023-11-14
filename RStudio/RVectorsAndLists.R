2b55d093eae24e4fd164be2484229e8928d61c93
Jeff <jstopp10@gmail.com>
2023-11-13 18:59:31 -0800

    Create RVectorsAndLists.R

---
+++ RStudio/RVectorsAndLists.R
@@ -0,0 +1,14 @@
+# Vector of strings
+veggies <- c("carrot", "broccoli", "lettuce")
+veggies
+# Access the first item(carrot)
+veggies[1]
+
+# Vector with numerical values in a sequence
+numbers <- 1:25
+numbers
+
+veggies <- c("carrot", "broccoli", "lettuce")
+# Change "carrot" to "onion"
+veggies[1] <- "onion"
+veggies
