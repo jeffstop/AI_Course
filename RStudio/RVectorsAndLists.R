e3ffcb6373c44afbfa6a28d11d8997d100ad1db2
Jeff <jstopp10@gmail.com>
2023-11-14 14:47:38 -0800

    Update RVectorsAndLists.R

--- RStudio/RVectorsAndLists.R
+++ RStudio/RVectorsAndLists.R
@@ -12,3 +12,42 @@ veggies <- c("carrot", "broccoli", "lettuce")
 # Change "carrot" to "onion"
 veggies[1] <- "onion"
 veggies
+
+thislist <- list("carrot", "broccoli", "lettuce")
+thislist
+thislist[1]
+
+thislist <- list("carrot", "broccoli", "lettuce")
+thislist[1] <- "onion"
+thislist
+
+# Create & print matrix
+mymatrix <- matrix(c(10, 20, 30, 40, 50, 60), nrow = 3, ncol = 2)
+mymatrix
+
+# Access items in matrix
+mymatrix <- matrix(c("onion", "broccoli", "lettuce", "tomato"), nrow = 2, ncol = 2)
+mymatrix
+mymatrix[2, 2]
+
+# Adding & removing rows and columns w/ rbind() & cbind() | r <- row; c <- column
+mymatrix <- matrix(c("onion", "broccoli", "lettuce", "tomato"), nrow = 2, ncol = 2)
+newmatrix <- cbind(mymatrix, c("cucumber", "spinach"))
+mymatrix
+newmatrix
+
+# Loop through a matrix
+mymatrix <- matrix(c("onion", "broccoli", "lettuce", "tomato"), nrow = 2, ncol = 2)
+for (rows in 1:nrow(mymatrix)) {
+  for (columns in 1:ncol(mymatrix)) {
+    print(mymatrix[rows, columns])
+  }
+}
+
+# Create matrices, then combine rows & columns
+FruitMatrix <- matrix(c("strawberry", "blueberry", "raspberry", "grape"), nrow = 2, ncol = 2)
+JuiceMatrix <- matrix(c("orange", "mango", "pineapple", "watermelon"), nrow = 2, ncol = 2)
+CombinedRows <- rbind(FruitMatrix, JuiceMatrix)
+CombinedRows
+CombinedColumns <- cbind(FruitMatrix, JuiceMatrix)
+CombinedColumns
